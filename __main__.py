be637fc65c21487881d648ef890839db83ea003e
ianertson <sebbekarlsson97@gmail.com>
2019-10-15 14:05:15 +0200

    init

---
+++ __main__.py
@@ -0,0 +1,29 @@
+import math
+from functools import reduce
+
+
+distance = lambda x0, y0, x1, y1: math.hypot(x1 - x0, y1 - y0)
+
+novelty = lambda d: 100 / max(1, d)
+
+combined_novelty = lambda x, y, cities: reduce(lambda s, p: s + novelty(distance(x, y, p[0], p[1])), cities, 0)
+
+most_novel_angle = lambda x, y, cities: reduce(
+    lambda i, j:\
+    j if combined_novelty(x+math.cos(j), y-math.sin(j), cities)\
+    >\
+    combined_novelty(x+math.cos(i), y-math.sin(i), cities) else i\
+    ,
+    range(0, 360)
+)
+
+print(most_novel_angle(
+    40,  # your x
+    120,  # your y
+    cities=[  # city coordinates
+        [0, 0],
+        [120, 0],
+        [200, 325],
+        [100, 55]
+    ]
+))
